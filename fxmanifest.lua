b3d206e2a79761a52dbbbee786b5f395e91c8d62
ghostrecom <53305213+ghostrecom@users.noreply.github.com>
2022-08-24 19:05:55 +0200

    test
    testando

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "adamant"
+games {"rdr3"}
+client_script {
+    "@uiprompt/uiprompt.lua",
+    'client/c_main.lua',
+    'config.lua',
+}
+server_script {
+    '@mysql-async/lib/MySQL.lua',
+    'server/s_main.lua',
+    'config.lua',
+}
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
